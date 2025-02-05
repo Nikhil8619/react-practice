59d13dca9536d0de5c8ad78bf0a2164264008f40
Nikhil8619 <nikhilagrawal546@gmail.com>
2025-02-05 17:49:20 +0530

    Daily Work Modified

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-const parent=React.createElement("div",{id:"parent"}, [
-    React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-    ),
-    React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-    )]
-);
-console.log(parent)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//const heading=React.createElement("h1",{"id":"heading"},"Hello React");  using react
+//React Element
+// const heading=<h1 className="head">Hello react using jsx</h1>
+
+//React functional component
+
+// const HeadingComponent=() => {
+//     return <h1 className="heading">Hey React functional components</h1>
+// }
+//Component composition
+const Title=() => (
+<h1 className="head">Hello react using jsx</h1>
+)
+
+// const title= (
+//     <h1 className="head">Hello react using jsx</h1>
+//     )
+
+const HeadingComponent=() =>(
+    <div id="container">
+        {/* <Title />   */}
+        {Title()}
+        {/* {title} */}
+       <h1 className="heading">Hey react functional components</h1>
+    </div>
+)
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Practice of React 
+Important Notes(Igniting react js imp for interview)
+
+#PARCEL(Usage and properties)[parcel.org]
+-Dev Build
+-creates a local server for the developer
+-supports HMR-hot module replacement
+-It supports File watching algorithm-written in c++
+-Caching-faster build the process
+-Helps in image optimization
+-Minification of files
+-Bundling of files
+-Compressing of files
+-consistent hashing
+-code splitting
+-differential bundling-supports older browser
+-Error Handling
+-Diagnostic
+-helps the user to host the project in HTTPS 
+-TREE SHAKING-remove unused code
+
+
